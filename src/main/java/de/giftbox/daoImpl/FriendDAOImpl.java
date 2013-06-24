906f57f309f1ced4914df99a3430c94be27a01bd
KonstantinPasternack <konstantin.pasternack@googlemail.com>
2013-06-25 00:02:21 +0200

    query test

--- src/main/java/de/giftbox/daoImpl/FriendDAOImpl.java
+++ src/main/java/de/giftbox/daoImpl/FriendDAOImpl.java
@@ -56,9 +56,10 @@ public class FriendDAOImpl implements FriendDAO {
 	@Transactional
 	public List<Benutzer> findFriendsByBenutzerId(Integer id) {
 		Session session = sessionFactory.getCurrentSession();
-		String sql = "SELECT b.* FROM benutzer b JOIN friend f ON b.id_Benutzer = f.benutzerFreund_fk WHERE f.benutzer_fk = "
-				+ id;
-		Query query = session.createSQLQuery(sql).addEntity(Benutzer.class);
+		String sql = "SELECT b.* FROM benutzer b JOIN friend f ON b.id_Benutzer = f.benutzerFreund_fk WHERE f.benutzer_fk = :id";
+		
+		Query query = session.createSQLQuery(sql);
+		query.setParameter("id", id);
 		// session.createQuery("SELECT b.* FROM benutzer b JOIN friend f ON b.id_Benutzer = f.benutzerFreund_fk WHERE f.benutzer_fk ="+id);
 
 		@SuppressWarnings("unchecked")
