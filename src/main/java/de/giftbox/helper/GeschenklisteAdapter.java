6b895b644b57dcaf46bf2d2a835fe1824243921b
chuzzla <e_realize@yahoo.de>
2013-06-25 02:34:11 +0200

    Geschenklisten id

--- src/main/java/de/giftbox/helper/GeschenklisteAdapter.java
+++ src/main/java/de/giftbox/helper/GeschenklisteAdapter.java
@@ -14,7 +14,7 @@ public class GeschenklisteAdapter implements JsonSerializer<Geschenkliste> {
     @Override
     public JsonElement serialize(Geschenkliste geschenkliste, Type type, JsonSerializationContext jsc) {
         JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("id_Geschenkliste", geschenkliste.getIdGeschenkliste());
+        jsonObject.addProperty("idGeschenkliste", geschenkliste.getIdGeschenkliste());
         jsonObject.addProperty("name", geschenkliste.getName());
         return jsonObject;      
     }
